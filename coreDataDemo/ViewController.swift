ac107d03e0e16b070fbb44b1e1d022810af959b0
Sarmad Nawaz <muhammadsarmadnawaz@gmail.com>
2024-11-10 17:57:34 +0500

    Initial Commit

---
+++ coreDataDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coreDataDemo
+//
+//  Created by sarmad on 11/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
